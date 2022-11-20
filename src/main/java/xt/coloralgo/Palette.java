64bc7625da0caddd4bb455657b28a9c1b4d4866c
Luc <luc_rousseau@hotmail.com>
2022-11-20 02:30:44 +0100

    Amelioration palette fractale 71

--- src/main/java/xt/coloralgo/Palette.java
+++ src/main/java/xt/coloralgo/Palette.java
@@ -90,7 +90,7 @@ public class Palette {
 	public static final Palette PALETTE_066 = new Palette(BLEU_CIEL, BLANC, OCRE);
 	public static final Palette PALETTE_068 = new Palette(AZUR, OCRE, BLANC, OCRE);
 	public static final Palette PALETTE_070 = new Palette(BLEU_FRANCE, BLEU_CIEL, BLANC, BORDEAUX);
-	public static final Palette PALETTE_071 = new Palette(BLEU_FRANCE, AZUR, BLANC, CAPUCINE);
+	public static final Palette PALETTE_071 = new Palette(NOIR, NOIR, AZUR, BLANC, CAPUCINE);
 	
 	public static final Palette PALETTE_VIDEO_001 = new Palette(BLEU, BLANC);
 	
