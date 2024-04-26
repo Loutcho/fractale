159290a55d7b42eb431267d7f8d7b79eacf12e3b
Luc <luc_rousseau@hotmail.com>
2024-04-26 22:58:50 +0200

    Slight modification fractal #102

--- src/main/java/xt/image/Image102.java
+++ src/main/java/xt/image/Image102.java
@@ -7,7 +7,6 @@ import xt.coloralgo.ApplyTestOrder;
 import xt.coloralgo.EscapeTimeAlgorithm;
 import xt.coloralgo.booleanexpression.AbsGreaterThan;
 import xt.coloralgo.booleanexpression.IGreaterThan;
-import xt.coloralgo.effect.Barycenter;
 import xt.coloralgo.effect.ConstantColor;
 import xt.coloralgo.effect.GridIm;
 import xt.coloralgo.effect.Inverse;
@@ -18,13 +17,14 @@ import xt.fractale.MathZone;
 import xt.math.Complex;
 
 /**
- * Flocons de neige bleu-rose
+ * Flocons de neige
  */
 public class Image102 {
 
+	private static final double U = 0.001005;
 	private static final double K = 1.0 / 3.0;
 	private static final double X = -0.366025;
-	private static final double Y = -1.358000;
+	private static final double Y = -1.364680;
 	
 	public static Image get() {
 		return new Image(new EscapeTimeAlgorithm(new xt.function.F39(K * Math.PI), new Complex(X, Y),
@@ -32,15 +32,10 @@ public class Image102 {
 				IF(new IGreaterThan(200),
 					new ConstantColor(0.0, 0.0, 0.0),
 					new Mul(
-						new Barycenter(
-							0.25,
-							new Inverse(new GridIm(0.50, 1.00, 0.60, 0.40)),
-							0.75,
-							new Inverse(new GridIm(0.25, 1.00, 0.60, 0.40))
-						),
-						new IterationGradient(0.35, 12.5)
+						new Inverse(new GridIm(0.25, 1.00, 0.60, 0.40)),
+						new IterationGradient(0.3, 15.5)
 					)
 				),
-				ApplyTestOrder.FIRST_TEST_THEN_APPLY), new MathZone(new Complex(0.0, 0.0), 1.5 * 0.0058, 0.0058, 0.0));
-	}
+				ApplyTestOrder.FIRST_TEST_THEN_APPLY), new MathZone(new Complex(0.0, 0.0), 1.5 * U, U, 0.0));
+		}
 }
